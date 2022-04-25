6497b48a11ceb53854aaf43deb3bcc47e692047e
dangnlh <nguyenlehaidang.290698@gmail.com>
2022-04-25 10:51:41 +0700

    first commit

---
+++ src/main/java/com/example/SpringBoot_CapstoneSystem_SWP/CapstoneSystem/CapstoneSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_CapstoneSystem_SWP.CapstoneSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBoot_CapstoneSystem_SWP/CapstoneSystem/CapstoneSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_CapstoneSystem_SWP.CapstoneSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
